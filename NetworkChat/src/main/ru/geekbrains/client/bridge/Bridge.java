f974e9cb32f5a07ce39de6b2737c3c216e17ce5e
chendyrevanastya <40794468+chendyreva@users.noreply.github.com>
2019-09-15 13:30:38 +0500

    Bridge and Decorator

---
+++ NetworkChat/src/main/ru/geekbrains/client/bridge/Bridge.java
@@ -0,0 +1,66 @@
+
+
+public class Bridge {
+    public static void main(String[] args) {
+        ICar car = new BMW(new CarBridge());
+        car.drive();
+
+        ICar track = new AudiTrack(new TrackBridge());
+        track.drive();
+
+    }
+}
+abstract class ICar{
+    IBridge iBridge;
+
+    public  ICar(IBridge iBridge) {
+        this.iBridge = iBridge;
+    }
+
+    public abstract  void drive();
+}
+
+abstract class ITrack {
+    abstract void drive();
+}
+
+interface IBridge {
+    void drive();
+}
+
+class CarBridge implements IBridge {
+    @Override
+    public void drive() {
+        System.out.println("drive car");
+    }
+}
+
+class TrackBridge implements IBridge {
+    @Override
+    public void drive() {
+        System.out.println("drive track");
+    }
+}
+
+
+class BMW extends ICar {
+    public BMW(IBridge iBridge) {
+        super(iBridge);
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("drive BMW(car)");
+    }
+}
+class AudiTrack extends ICar {
+    public AudiTrack(TrackBridge trackBridge) {
+        super(trackBridge);
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("drive audi(track)");
+    }
+}
+
