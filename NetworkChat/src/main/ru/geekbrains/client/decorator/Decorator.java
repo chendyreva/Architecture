4e34c10bfd9660259048a7e20464790ca3e90907
chendyrevanastya <40794468+chendyreva@users.noreply.github.com>
2019-09-21 22:10:09 +0500

    Chain/ Command/ Observer

---
+++ NetworkChat/src/main/ru/geekbrains/client/decorator/Decorator.java
@@ -0,0 +1,40 @@
+ package ru.geekbrains.client.decorator;
+
+public class Decorator {
+    public static void main(String[] args) {
+        FileStream fileStream = new FileBufferedReader(new FileStreamReader());
+        fileStream.read();
+    }
+}
+interface FileStream {
+
+    void read();
+}
+class FileStreamReader implements  FileStream {
+    @Override
+    public void read() {
+        System.out.println("read file");
+    }
+}
+
+abstract class FileDecorator implements FileStream {
+    FileStream fileDecorator;
+
+    public FileDecorator(FileStream fileDecorator) {
+        this.fileDecorator = fileDecorator;
+    }
+
+    public abstract  void read();
+}
+
+class FileBufferedReader extends FileDecorator {
+    public FileBufferedReader(FileStream fileDecorator) {
+        super (fileDecorator);
+    }
+
+    @Override
+    public void read() {
+        fileDecorator.read();
+        System.out.println("buffered read");
+    }
+}
