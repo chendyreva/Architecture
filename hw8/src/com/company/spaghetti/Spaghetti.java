36a1a8c62304ad0feb870f9036e3cf78ac859ac6
chendyrevanastya <40794468+chendyreva@users.noreply.github.com>
2019-10-05 21:24:28 +0500

    examples of antipatterns

---
+++ hw8/src/com/company/spaghetti/Spaghetti.java
@@ -0,0 +1,40 @@
+package com.company.spaghetti;
+
+public class Spaghetti {
+//  не линейный код
+//    do_a();
+//if (check) {
+//        something();
+//    } else {
+//        anything();
+//        if (whatever()) {
+//            for (a in b) {
+//                if (good(a)) {
+//                    something();
+//                }
+//            }
+//        }
+    /*
+    bad exmp
+
+    void printOwing() {
+  printBanner();
+
+ /.../
+  System.out.println("name: " + name);
+  System.out.println("amount: " + getOutstanding());
+}
+
+good exmp
+
+void printOwing() {
+  printBanner();
+  printDetails(getOutstanding());
+}
+
+void printDetails(double outstanding) {
+  System.out.println("name: " + name);
+  System.out.println("amount: " + outstanding);
+}
+     */
+}
